11d9c3cc34ab127d5b96d884fb2504da081807d8
Reyaad Rafeek <reyaad510@gmail.com>
2019-05-31 16:32:37 -0400

    .get dish by id functionality completed

--- routers/dishes.js
+++ routers/dishes.js
@@ -7,8 +7,26 @@ const db = require('../data/helper.js')
 // Get Dishes
 // /api/dishes
 router.get('/', async(req, res) => {
-    const dishes = await db.getDishes();
-    res.status(200).json(dishes);
+    try {
+        const dishes = await db.getDishes();
+        res.status(200).json(dishes);
+        } catch(error) {
+            res.status(500).json({message: 'Error retreiving dishes'})
+    }
+})
+
+//Get Dish by ID
+router.get('/:id', async(req, res) => {
+    try {
+        const dish = await db.getDish(req.params.id);
+        if(dish) {
+            res.status(200).json(dish);
+        } else {
+            res.status(404).json({ message: 'Could not find specific dish. :(' })
+        }
+    } catch(error) {
+        res.status(500).json({message:'Error retrieving dish'})
+    }
 })
 
 
