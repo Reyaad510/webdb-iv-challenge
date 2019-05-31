0b3e060b441aa0ced2575b095e6eed185594b25f
Reyaad Rafeek <reyaad510@gmail.com>
2019-05-31 11:55:41 -0400

    Scaffold dish router

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const dishRouter = require('./routers/dishes.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
+server.use('/api/dishes', dishRouter);
 
 
 server.get('/', (req, res) => {
