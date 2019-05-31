d25fae41daf1e6f1a5022e6d9c82942da8a698ea
Reyaad Rafeek <reyaad510@gmail.com>
2019-05-31 12:04:03 -0400

    Created knexfile and knexConfig

--- routers/dishes.js
+++ routers/dishes.js
@@ -1,4 +1,17 @@
+const knex = require('knex');
 const router = require('express').Router();
+
+
+
+const knexConfig = {
+    client: 'sqlite3',
+    connection: {
+        filename: './data/recipe.db3'
+    },
+    useNullAsDefault: true,
+}
+
+const db = knex(knexConfig);
 
 router.get('/', async(req, res) => {
     res.status(200).json({message: 'Test Dish Router'})
